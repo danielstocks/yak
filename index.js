4fd9c4183c1298feb7bc324bf4eb28cebd2663e0
Daniel Stocks <daniel@webcloud.se>
2015-09-25 12:08:03 +0200

    HTTP headers and API changes
    - Now possible to send HTTP headers via all static and instance methods
    - Added option to add query params in GET requests via `where` option
    - Added installation instructions to README
    - Double quote cleanup
    - Bumped version to 0.4

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var Model = require('./lib/model');
+var model = require('./lib/model');
 
 module.exports = function(options) {
-
-  Model.yak = this;
-  this.Model = Model;
+  this.model = model;
   this.headers = {
     'Content-Type': 'application/json'
   }
   this.host = options.host;
-
-  this.setHeaders = function(options) {
-    Object.assign(this.headers, options);
-  }
 }
